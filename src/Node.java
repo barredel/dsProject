490c31406a28d3891de45628192c85e82cfe4161
barredel <bar.redel@campus.technion.ac.il>
2023-01-16 16:36:07 +0200

    z

--- src/Node.java
+++ src/Node.java
@@ -1,5 +1,38 @@
-public class Node extends Leaf
+public class Node
 {
+    protected Node parent;
+    protected int primaryKey;
+    protected int secondaryKey;
+
+    public Node (int primaryKey, int secondaryKey)
+    {
+        this.primaryKey = primaryKey;
+        this.secondaryKey = secondaryKey;
+        this.parent = null;
+    }
 
 
+    public int getPrimaryKey() {
+        return primaryKey;
+    }
+
+    public int getSecondaryKey() {
+        return secondaryKey;
+    }
+
+    public void setPrimaryKey(int primaryKey) {
+        this.primaryKey = primaryKey;
+    }
+
+    public void setSecondaryKey(int secondaryKey) {
+        this.secondaryKey = secondaryKey;
+    }
+
+    public Node getParent() {
+        return parent;
+    }
+
+    public void setParent(Node parent) {
+        this.parent = parent;
+    }
 }
